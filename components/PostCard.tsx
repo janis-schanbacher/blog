ee44824968c7f4c4bee30191478adef060f3141e
Janis Schanbacher <janis.schanbacher@posteo.de>
2022-05-30 20:59:28 +0200

    Fetch posts from graphcms and display previews

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from 'react';
+import moment from 'moment';
+import Link from 'next/link';
+import { Avatar, Card } from 'antd';
+import { CalendarOutlined } from '@ant-design/icons';
+const { Meta } = Card;
 
-const PostCard = ({post}) => {
+const PostCard = ({ post }) => {
+  console.log(post);
+
   return (
-    <div >
-      {post.title}
-      {post.excerpt}
-    </div>
+    <Link href={`/posts/${post.slug}`}>
+      <Card
+        hoverable
+        cover={<img alt={post.title} src={post.featuredImage.url} />}
+        style={{ margin: '16px' }}
+      >
+        <Meta
+          avatar={<Avatar src={post.author.photo.url} alt={post.author.name} />}
+          title={post.title}
+          description={post.excerpt}
+        />
+        <div style={{ marginTop: '16px', float: 'right' }}>
+          <CalendarOutlined /> {moment(post.createdAt).format('MMM DD, YYYY')}
+        </div>
+      </Card>
+    </Link>
   )
 }
 
